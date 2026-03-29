abca8d5af3329f2c01bf3c29bb43fad323ac9bfe
Mythili Sivasubramanian <sivasubramanianmythili@gmail.com>
2026-03-29 22:43:55 +0200

    Add strings_foundations.py

---
+++ 01_Python_Labs/strings_foundations.py
@@ -0,0 +1,9 @@
+"""
+Basic string indexing and slicing
+"""
+
+s = "python programming"
+
+print(f"First character: {s[0]}")
+print(f"Last character: {s[-1]}")
+print(f"Sliced word ('python'): {s[:6]}")
