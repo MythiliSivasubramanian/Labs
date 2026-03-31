ccea695dce0637b42c3abe17647c1a88c7f05b03
Mythili Sivasubramanian <sivasubramanianmythili@gmail.com>
2026-03-31 22:39:33 +0200

    Add practice on Strings

--- 01_Python_Labs/strings_foundations.py
+++ 01_Python_Labs/strings_foundations.py
@@ -32,3 +32,17 @@ print(f"Original string: '{s2}'")
 print(f"After strip: '{cleaned}'")
 print(f"After lowercase: '{lowered}'")
 print(f"Final output: '{final}'")
+
+
+s3 = "python is easy and python is powerful"
+"""
+- Count how many times "python" appears
+- Find the first occurrence index of "python"
+- Replace only the first occurrence of "python" with "Java"
+- Convert the whole string into a list of words
+"""
+print(f"\nThe original String : {s3}\n")
+print(f"\nCount how many times 'python' appears : {s3.count("python")}\n")
+print(f"Find the first occurrence index of 'python' : {s3.find('python')}\n")
+print(f"Replace only the first occurrence of 'python' with 'Java' : {s3.replace("python","Java", 1)}\n")
+print(f"Convert the whole string into a list of words : {s3.split()}\n")
